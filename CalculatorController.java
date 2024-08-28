6b7fba518e1676e5cbf003d8896e4714fafd86fb
Pete Coutros <118582425+Pcoutros@users.noreply.github.com>
2024-08-28 00:16:49 -0500

    Update CalculatorController.java

--- CalculatorController.java
+++ CalculatorController.java
@@ -1,10 +1,13 @@
-
+/**
+* This class will be the controller for the MVC and will pass info between the view and model
+*/
 public class CalculatorController {
 
 	private CalculatorModel model;
 	private CalculatorView view;
 	
 	public CalculatorController(CalculatorModel model, CalculatorView view) {
+		
 		this.model = model;
 		this.view = view;
 	}
